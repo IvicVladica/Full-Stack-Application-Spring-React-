b52bf0e6fa397f0f3a1d1475a42e76696f112724
IvicVladica <ivicvladica@gmail.com>
2025-01-22 11:14:20 +0100

    First commit

---
+++ src/main/java/com/crm/crm/model/ContactRepository.java
@@ -0,0 +1,10 @@
+package com.crm.crm.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContactRepository extends CrudRepository<Contact, Long> {
+    
+}
+
+    
+
